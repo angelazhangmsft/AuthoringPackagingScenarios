6f25c413e16a06d0d35cf77af2ea01070314970c
Angela Zhang <angzhang@microsoft.com>
2021-03-15 17:08:57 -0700

    Add project files.

---
+++ CppConsoleApp/main.cpp
@@ -0,0 +1,15 @@
+﻿#include "pch.h"
+#include <iostream>
+
+using namespace winrt;
+using namespace Windows::Foundation;
+using namespace std;
+
+int main()
+{
+    init_apartment();
+    AuthoringDemo::Example ex;
+    ex.SampleProperty(42);
+    wcout << ex.SampleProperty() << endl;
+    wcout << ex.SayHello().c_str() << endl;
+}
